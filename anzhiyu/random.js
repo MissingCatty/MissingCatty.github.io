5ece2b1d8215eaff35cdbe6c58afd79ca57de1bf
OMENZYC\13566 <1>
2024-12-31 22:10:19 +0800

    Site updated: 2024-12-31 22:10:18

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/19/hello-world/","2024/12/30/algorithm/两数之和/","2024/12/30/algorithm/寻找两个有序数组的中位数/"];function toRandomPost(){
+var posts=["2024/11/19/hello-world/","2024/12/30/algorithm/两数之和/","2024/12/31/algorithm/寻找两个有序数组的中位数/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
