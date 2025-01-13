d8ff115c4d7915a38eb957c6bfb5b8843177630a
OMENZYC\13566 <1>
2025-01-13 11:38:23 +0800

    Site updated: 2025-01-13 11:38:22

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/31/algorithm/寻找两个有序数组的中位数/","2024/12/30/algorithm/两数之和/","2025/01/11/algorithm/移除元素/","2025/01/11/algorithm/有序数组的平方/","2025/01/11/algorithm/长度最小子数组/","2025/01/12/algorithm/区间和/"];function toRandomPost(){
+var posts=["2024/12/31/algorithm/寻找两个有序数组的中位数/","2024/12/30/algorithm/两数之和/","2025/01/11/algorithm/移除元素/","2025/01/11/algorithm/有序数组的平方/","2025/01/11/algorithm/长度最小子数组/","2025/01/12/algorithm/区间和/","2025/01/13/algorithm/开发商购买土地/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
